0fe88b89ff9ef7ed10cc102aed41fc4e68d860d8
Lars Baunwall <larslb@thinkability.dk>
2021-01-18 13:38:05 +0100

    initial

---
+++ Client/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:44330");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+            }
+            
+            // request token
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+
+                ClientId = "platformApiClient",
+                ClientSecret = "SuperSecretPassword",
+                Scope = "rabbitmq.read:*/*"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.Read();
+        }
+    }
+}
---
+++ Server/Startup.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace IdentityServer_Example
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer()
+                .AddInMemoryClients(new List<Client>
+                {
+                    new Client
+                    {
+                        ClientId = "platformApiClient",
+                        ClientName = "Example client application",
+                        AllowedGrantTypes = GrantTypes.ClientCredentials,
+                        ClientSecrets = new List<Secret> {new Secret("SuperSecretPassword".Sha256())}, // change me!
+                        AllowedScopes = new List<string> {"rabbitmq.read:*/*"}
+                    }
+                })
+                .AddInMemoryApiResources(new List<ApiResource>
+                {
+                    new ApiResource
+                    {
+                        Name = "rabbitmq",
+                        DisplayName = "Quasar messaging",
+                        Scopes = new List<string> {"rabbitmq.read:*/*", "api1.write"},
+                    }
+                })
+                .AddInMemoryApiScopes(new List<ApiScope>()
+                {
+                    new ApiScope("rabbitmq.read:*/*", "Read access to RabbitMQ"),
+                })
+                .AddDeveloperSigningCredential();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseIdentityServer();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+            });
+        }
+    }
+}
